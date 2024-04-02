71e703dbba171e17a1b4f6b03edb033856ce13a3
sszf <15620182662@163.com>
2024-04-01 21:57:33 -0400

    remove countOf and test method

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -27,25 +27,25 @@ public class ArrayUtilsTest {
         assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
     }
 
-//    @Test
-//    public void testOddOrPosNull() {
-//        // Do not execute fault
-//        assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
-//    }
-//
-//
-//    @Test
-//    public void testOddOrPosAllPositives() {
-//        // Any nonempty x with only non-negative elements works, because the first part of the
-//        // compound if-test is not necessary unless the value is negative.
-//        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
-//    }
-//
-//    @Test
-//    public void testOddOrPositiveBothPositivesAndNegatives() {
-//        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
-//    }
-//
+    @Test
+    public void testOddOrPosNull() {
+        // Do not execute fault
+        assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
+    }
+
+
+    @Test
+    public void testOddOrPosAllPositives() {
+        // Any nonempty x with only non-negative elements works, because the first part of the
+        // compound if-test is not necessary unless the value is negative.
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testOddOrPositiveBothPositivesAndNegatives() {
+        assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
+    }
+
 //    @Test
 //    public void testCountOfEmptyArray(){
 //        assertEquals(0, ArrayUtils.countOf(new int[]{}, 0));
